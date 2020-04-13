6dadb4b090c69e044d1bc06a52d4e9f380c60179
ArturDead <arturdead@localhost.localdomain>
2020-04-14 02:15:25 +0700

    upgrade hangman:3.c and add few marks

--- hangman:3.c
+++ hangman:3.c
@@ -30,15 +30,19 @@ int get_variant(int count){
     return variant;
 }
 
+void print_hang() {
+    printf("Валера\n");
+}
+
 int main(){
     int variant; // выбранный пункт меню
 
     do {
 	print_menu(); // выводим меню на экран
-	variant = get_variant(5); // получаем номер выбранного пункта меню 
+	variant = get_variant(5); // получаем номер выбранного пункта меню
 	switch (variant) {
 	    case 1:
-		// здесь будет функция Валеры
+		print_hang();// здесь будет функция Валеры
 		break;
 	    case 2:
 		print_word();
@@ -47,8 +51,8 @@ int main(){
 		print_list();
 		break;
        }
-       if (variant != 5)
-	    system("pause"); //зажерживаем выполнение, чтобы пользователь мог увидеть результат работы выбранного пункта
-	} while (variant != 5);
+       if (variant != 4)
+	    system("pause"); // задерживаем выполнение, чтобы пользователь мог увидеть резульат выбранного пункта
+	} while (variant != 4);
 	return 0;
 }
