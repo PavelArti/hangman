09312165ac4799d290bde545fb18b9223db0a835
ValeraMilovanov <milovanov.2001@mail.ru>
2020-04-16 21:08:05 +0700

    Add image output hang

--- hangman:3.c
+++ hangman:3.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_menu() {
+void print_menu()
+{
     system("cls"); // очищаем экран
     printf("What do you whant to do ?\n");
     printf("1. Start game\n");
@@ -10,49 +11,86 @@ void print_menu() {
     printf("4. Exit\n");
     printf(">");
 }
-void print_word() {
+void print_word()
+{
     printf("Виселица это хорошо я оформлю текст позже\n");
 }
-void print_list() {
+void print_list()
+{
     printf("Список команд потом оформлю\n");
 }
 
-int get_variant(int count){
+int get_variant(int count)
+{
     int variant;
     char s[100]; // строчка для считывания введенных данных
     scanf("%s", s); // считываем строку
 
     //пока ввод некоректен, сообщаем об этом и просим повторить его
-    while (sscanf(s, "%d", &variant) !=1 || variant < 1 || variant > count) {
-	printf("Incorrect input. Try again: "); // выводим сообщение об ошибке 
-	scanf("%s", s); //считываем строку повторно
+    while (sscanf(s, "%d", &variant) != 1 || variant < 1 || variant > count) {
+        printf("Incorrect input. Try again: "); // выводим сообщение об ошибке
+        scanf("%s", s); //считываем строку повторно
     }
     return variant;
 }
 
-void print_hang() {
-    printf("Валера\n");
+void print_hang(int number_of_mistakes) //вывод изображения виселицы
+{
+    switch (number_of_mistakes) {
+    case 9:
+        printf("  ____ \n |   | |\n |   O |\n |  /|\\|\n_|_ / \\|  \n\n");
+        break;
+    case 8:
+        printf("  ____ \n |   | |\n |   O |\n |  /|\\|\n_|_ /  |  \n\n");
+        break;
+    case 7:
+        printf("  ____ \n |   | |\n |   O |\n |  /|\\|\n_|_    |  \n\n");
+        break;
+    case 6:
+        printf("  ____ \n |   | |\n |   O |\n |  /| |\n_|_    |  \n\n");
+        break;
+    case 5:
+        printf("  ____ \n |   | |\n |   O |\n |   | |\n_|_    |  \n\n");
+        break;
+    case 4:
+        printf("  ____ \n |   | |\n |   O |\n |     |\n_|_    |  \n\n");
+        break;
+    case 3:
+        printf("  ____ \n |     |\n |     |\n |     |\n_|_    |  \n\n");
+        break;
+    case 2:
+        printf("       \n |     |\n |     |\n |     |\n_|_    |  \n\n");
+        break;
+    case 1:
+        printf("       \n       |\n       |\n       |\n___    |  \n\n");
+        break;
+    case 0:
+        printf("       \n       |\n       |\n       |\n       |  \n\n");
+        break;
+    };
 }
 
-int main(){
+int main()
+{
     int variant; // выбранный пункт меню
 
     do {
-	print_menu(); // выводим меню на экран
-	variant = get_variant(5); // получаем номер выбранного пункта меню
-	switch (variant) {
-	    case 1:
-		print_hang();// здесь будет функция Валеры
-		break;
-	    case 2:
-		print_word();
-		break;
-	    case 3:
-		print_list();
-		break;
-       }
-       if (variant != 4)
-	    system("pause"); // задерживаем выполнение, чтобы пользователь мог увидеть резульат выбранного пункта
-	} while (variant != 4);
-	return 0;
+        print_menu(); // выводим меню на экран
+        variant = get_variant(5); // получаем номер выбранного пункта меню
+        switch (variant) {
+        case 1:
+            print_hang(); // здесь будет функция Валеры
+            break;
+        case 2:
+            print_word();
+            break;
+        case 3:
+            print_list();
+            break;
+        }
+        if (variant != 4)
+            system("pause"); // задерживаем выполнение, чтобы пользователь мог
+                             // увидеть резульат выбранного пункта
+    } while (variant != 4);
+    return 0;
 }
