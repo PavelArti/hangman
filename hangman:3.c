901a1aff22842744321393e95927f38a7827238a
ValeraMilovanov <milovanov.2001@mail.ru>
2020-05-08 15:14:25 +0700

    Add function hangman

--- hangman:3.c
+++ hangman:3.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <time.h>
 
 void print_menu()
 {
-    system("clear"); // очищаем экран
+    system("cls"); // очищаем экран
     printf("What do you whant to do ?\n");
     printf("1. Start game\n");
     printf("2. Description\n");
@@ -13,10 +15,7 @@ void print_menu()
 }
 void print_word()
 {
-    printf("Игра Виселица (hangman) это классическая  популярная во всем мире игра.\n");
-    printf("Вам необходимо отгодать скрытое слово по буквам.\n");
-    printf("На это вам дается 6 попыток и за каждую ошибку на экране будет появляться часть человека на виселеце\n");
-    printf("Если человек будет полностью нарисован, вы проиграете\n");
+    printf("Виселица это хорошо я оформлю текст позже\n");
 }
 void print_list()
 {
@@ -72,6 +71,104 @@ void print_hang(int number_of_mistakes) //вывод изображения ви
         break;
     };
 }
+int hangman()
+{
+    system("chcp 1251");
+    system("cls");
+    FILE* txt;
+    char word[80], enterLetter[33] = {0}, scanChar;
+    double buffer;
+    int i, flag = 0, sizeFile = 0, rndStr, sizeWord, guessChar,
+           number_of_mistakes = 0, countLetter = 0;
+    srand(time(NULL));
+    if (!(txt
+          = fopen("slova.txt", "r"))) { // проверяем открыт ли файл со словами
+        printf("Ошибка открытия файла!");
+        return 1;
+    }
+    while (fscanf(txt, "%s", &buffer)
+           != EOF) // получаем количество строк к файле со словами
+        sizeFile++;
+    rndStr = rand() % sizeFile; // получаем случайный номер строки из файла
+    fseek(txt, 0, SEEK_SET); // переводим курсор на начало файла
+    for (i = 0; i < rndStr; i++) { // получаем случайную строку с номером rndStr
+                                   // (это будет загаданное слово)
+        fscanf(txt, "%s", word);
+    }
+    fclose(txt);
+    sizeWord = strlen(word); // плучаем количество букв в строке
+    char* secretWord = (char*)calloc(
+            sizeWord,
+            sizeof(char)); // динамически задаём второй массив, в котором будут
+                           // показываться отк=гаданные буквы
+    memset(secretWord, '_', sizeWord); // заполняем его нижними подчёркиваниями
+    guessChar = sizeWord;
+    while (guessChar >= 0 && number_of_mistakes <= 9) {
+        if (flag == 1) {
+            number_of_mistakes++;
+        } // флаг равен 1 при неотгаданной букве
+        system("cls");
+        print_hang(number_of_mistakes);
+        if (guessChar
+            == 0) { // если количество неотгаданных букв = 0, то победа
+            printf("%s", word);
+            printf("\nВы победили!");
+            free(secretWord);
+            getchar();
+            return 0;
+        }
+        if (number_of_mistakes == 9) { // если количество ошибочно отгаданных
+                                       // букв = 9, то поражение
+            printf("%s", word);
+            printf("\nВы проиграли(");
+            free(secretWord);
+            getchar();
+            return 0;
+        }
+        printf("%s", secretWord);
+        printf("\nВведённые буквы:\n");
+        printf("%s", enterLetter);
+        printf("\nВведите букву: ");
+        scanChar = getchar();
+        getchar();
+        if (!((((int)scanChar >= -64 && (int)scanChar <= -33)
+               || (int)scanChar == -88)
+              || (((int)scanChar >= -32 && (int)scanChar <= -1)
+                  || (int)scanChar == -72))) { // символы не русского алфавита
+                                               // не учитываются
+            continue;
+        } else if ((int)scanChar >= -32 && (int)scanChar <= -1) { // трочные
+                                                                  // буквы
+                                                                  // преобразуются
+                                                                  // в прописные
+            scanChar = (char)((int)scanChar - 32);
+        } else if ((int)scanChar == -72) { // для буквыы ё
+            scanChar = (char)(-88);
+        }
+        flag = 1;
+        for (i = 0; i < sizeWord;
+             i++) { // цикл проверяет каждую букву на соответствие введённой
+            if (word[i] == scanChar) {
+                if (!(
+                            strchr(secretWord + i,
+                                   scanChar))) { // если такая буква ещё не была
+                                                 // отгадана, то выполняется
+                    flag = 0;
+                    guessChar--;
+                    secretWord[i] = word[i];
+                }
+            } else if (strchr(enterLetter,
+                              scanChar)) { // если буква уже была введена
+                flag = 0;
+            }
+        }
+        if (!(strchr(
+                    enterLetter, scanChar))) { // запись в массив введённых букв
+            enterLetter[countLetter++] = scanChar;
+        }
+    }
+    free(secretWord);
+}
 
 int main()
 {
@@ -82,7 +179,7 @@ int main()
         variant = get_variant(5); // получаем номер выбранного пункта меню
         switch (variant) {
         case 1:
-            print_hang(); // здесь будет функция Валеры
+            hangman(); // здесь будет функция Валеры
             break;
         case 2:
             print_word();
