e03e86ca32e39696e28aaf04a7a919f5b27c1c5e
ValeraMilovanov <milovanov.2001@mail.ru>
2020-06-09 19:07:53 +0700

    Function Hangman changed

--- hangman.c
+++ hangman.c
@@ -75,115 +75,73 @@ void print_hang(int number_of_mistakes)
 }
 void hangman()
 {
-    setlocale(LC_ALL, "Rus");
-    system("clear");
-    /*FILE *txt = fopen("slova.txt", "r");
-    if (txt == NULL)
-    {
-        printf("file open error");
-    }*/
-
+    char word[20];
+    char stars[20];
+    int counter = 0;
+    int lenght;
     char scanChar;
-    int i, flag = 0, sizeFile = 0, rndStr, sizeWord, guessChar;
-    int number_of_mistakes = 0, countLetter = 0;
-    char enterLetter[33] = {0};
-    /*srand(time(NULL));
-    // получаем количество строк к файле со словами
-    while (!feof(txt))
-    {
-        if (fgetc(txt) == '\n')
-            sizeFile++;
-    }*/
-    // rndStr = rand() % sizeFile; // получаем случайный номер строки из файла
-    // fseek(txt, 0, SEEK_SET);    // переводим курсор на начало файла
-    // получаем случайную строку с номером rndStr
-    // это будет загаданное слово
-    // for (i = 0; i < rndStr; i++)
-    //{
-    //    fscanf(txt, "%s", word);
-    //}
-    // fclose(txt);
-    // sizeWord = strlen(word); // плучаем количество букв в строке
+    int strikes = 0;
+    int trigger = 0;
+    int wincounter = 0;
+    int i;
 
-    char word[] = "СЛОВО";
-    sizeWord = 5;
+    printf(" _                                             \n");
+    printf("| |                                            \n");
+    printf("| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  \n");
+    printf("| '_ \\ / _` | '_ \\ / _` | '_ ` _ \\ / _` | '_ \\ \n");
+    printf("| | | | (_| | | | | (_| | | | | | | (_| | | | | \n");
+    printf("|_| |_|\\__,_|_| |_|\\__, |_| |_| |_|\\__,_|_| |_| \n");
+    printf("                    __/ |                      \n");
+    printf("                   |___/  \n");
 
-    // int random_word(char word); //ФУНКЦИЯ ВЫБОРА СЛОВА, КОТОРОЕ БУДУТ
-    // ОТГАДЫВАТЬ. ДОЛЖНА ВОЗВРАЩАТЬ КОЛИЧЕСТВО БУКВ В СЛОВЕ
-    // НА ВХОД ПОЛУЧАЕТ УКАЗАТЕЛЬ НА БУФЕР, В КОТОРОМ БУДЕТ СЛОВО
+    printf("\n\nEnter a word:");
+    scanf("%s", word);
+    lenght = strlen(word);
 
-    char* secretWord = (char*)calloc(sizeWord, sizeof(char));
-    // динамически задаём второй массив, в котором будут
-    // показываться отгаданные буквы
-    memset(secretWord, '_', sizeWord); // заполняем нижними подчёркиваниями
-    memset(enterLetter, '_', 9 + sizeWord);
-    guessChar = sizeWord;
-    while (guessChar >= 0 && number_of_mistakes <= 9) {
-        if (flag == 1) {
-            number_of_mistakes++;
-        } // флаг равен 1 при неотгаданной букве
-        system("clear");
-        print_hang(number_of_mistakes);
-        printf("\nThe letter:\n");
-        printf("%s", secretWord);
-        printf("\nEnter a leter: ");
-        printf("%s\n", enterLetter);
-        scanChar = getchar();
-        getchar();
-        if (guessChar == 0) {
-            printf("%s", word);
-            // если количество неотгаданных букв = 0, то победа
-            printf("\nYou win!");
-            sleep(3);
-            free(secretWord);
-            getchar();
+    system("clear");
+
+    for (counter = 0; counter < lenght; counter++) {
+        stars[counter] = '*';
+    }
+
+    stars[lenght] = '\0';
+
+    for (counter = 0; counter < 26; counter++) {
+        if (wincounter == lenght) {
+            printf("\n\nThe word was: %s\n", word);
+            printf("\nYou win!\n");
+            break;
         }
-        // если количество ошибочно отгаданных
-        // букв = 9, то поражение
-        if (number_of_mistakes == 9) {
-            printf("%s", word);
-            printf("\nYou lose(");
-            sleep(3);
-            free(secretWord);
-            getchar();
+
+        PrintHang(strikes);
+
+        if (strikes == 6) {
+            printf("\n\nThe word was: %s\n", word);
+            printf("\n\nYou lose.\n");
+            break;
         }
-        // символы не русского алфавита
-        // не учитываются
-        if (!((((int)scanChar >= -64 && (int)scanChar <= -33)
-               || (int)scanChar == -88)
-              || (((int)scanChar >= -32 && (int)scanChar <= -1)
-                  || (int)scanChar == -72))) {
-            continue;
-            //строчные буквы преобразуются в прописные
-        } else if ((int)scanChar >= -32 && (int)scanChar <= -1) {
-            scanChar = (char)((int)scanChar - 32);
-        } else if ((int)scanChar == -72) {
-            scanChar = (char)(-88);
-        }
-        // для буквы ё
-        flag = 1;
-        // цикл проверяет каждую букву на соответствие введённой
-        for (i = 0; i < sizeWord; i++) {
+
+        printf("\n\n\n\n%s", stars);
+
+        printf("\n\nGuess a letter:");
+        scanf(" %c", &scanChar);
+
+        for (i = 0; i < lenght; i++) {
             if (word[i] == scanChar) {
-                // если такая буква ещё не была отгадана, то выполняется
-                if (!(strchr(secretWord + i, scanChar))) {
-                    flag = 0;
-                    guessChar--;
-                    secretWord[i] = word[i];
-                }
-                // если буква уже была введена
-            } else if (!(strchr(enterLetter, scanChar))) {
-                flag = 0;
+                stars[i] = scanChar;
+                trigger++;
+                wincounter++;
             }
         }
-        // запись в массив введённых букв
-        if (!(strchr(enterLetter, scanChar))) {
-            enterLetter[countLetter++] = scanChar;
-        }
-    }
-    free(secretWord);
-}
 
+        if (trigger == 0) {
+            strikes++;
+        }
+
+        trigger = 0;
+    }
+    sleep(3);
+}
 int main()
 {
     setlocale(LC_ALL, "Rus");
